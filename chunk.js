03e8e4f69a8187adef77cde0e3a7fbd394ae4661
Koreex Jin <koreex.jin@gmail.com>
2022-02-13 02:40:21 +0800

    pass parameters to generate density function

--- chunk.js
+++ chunk.js
@@ -22,6 +22,15 @@ export default class Chunk extends THREE.Mesh {
         this.yUnitSize = 4;
         this.material = terrainMaterial;
 
+        this.noiseScale = 3;
+        this.octaves = 8;
+        this.persistence = 1.15;
+        this.lacunarity = 1.6;
+        this.floorOffset = 20.0;
+        this.hardFloor = 2.0;
+        this.hardFloorWeight = 3.05;
+        this.noiseWeight = 6.09;
+
         this.build();
     }
 
@@ -43,7 +52,11 @@ export default class Chunk extends THREE.Mesh {
     }
 
     build() {
-        const output = this.geometryUtils.generateChunk(this.origin.x, 0, this.origin.z, this.boundsSize);
+        const output = this.geometryUtils.generateChunk(
+            this.origin.x, 0, this.origin.z, this.boundsSize,
+            this.noiseScale, this.octaves, this.persistence, this.lacunarity, this.floorOffset,
+            this.hardFloor, this.hardFloorWeight, this.noiseWeight
+        );
 
         this.geometry.setAttribute('position', new THREE.Float32BufferAttribute(output.positions, 3));
         this.geometry.setIndex(new THREE.Uint32BufferAttribute(output.faces, 1));
