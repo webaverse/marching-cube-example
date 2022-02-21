80ae6eaa25970ce316094a81082ab4b6931e9c11
Koreex Jin <koreex.jin@gmail.com>
2022-02-21 17:07:31 +0800

    implement dynamic allocation

--- standalone/terrain-manager.js
+++ standalone/terrain-manager.js
@@ -12,7 +12,7 @@ export class TerrainManager {
 		this.chunkCount = this.chunkRange * 2 + 1;
 
 		this.targetChunkIds = this._calculateTargetChunks();
-		this.currentChunkIds = [...this.targetChunkIds];
+		this.currentChunks = this.targetChunkIds.map((v, i) => { return { slots: [i, i], chunkId: v } });
 
 		this.segment = 32;
 
@@ -42,15 +42,19 @@ export class TerrainManager {
 
 		this.positionBuffer = this.moduleInstance.HEAP32.subarray(head + 0, head + 1)[0];
 		this.normalBuffer = this.moduleInstance.HEAP32.subarray(head + 1, head + 2)[0];
-		this.faceBuffer = this.moduleInstance.HEAP32.subarray(head + 2, head + 3)[0];
-		this.groupBuffer = this.moduleInstance.HEAP32.subarray(head + 3, head + 4)[0];
+		this.indexBuffer = this.moduleInstance.HEAP32.subarray(head + 2, head + 3)[0];
+		this.chunkVertexRangeBuffer = this.moduleInstance.HEAP32.subarray(head + 3, head + 4)[0];
+		this.vertexFreeRangeBuffer = this.moduleInstance.HEAP32.subarray(head + 4, head + 5)[0];
+		this.chunkIndexRangeBuffer = this.moduleInstance.HEAP32.subarray(head + 5, head + 6)[0];
+		this.indexFreeRangeBuffer = this.moduleInstance.HEAP32.subarray(head + 6, head + 7)[0];
 
 		this.positions = this.moduleInstance.HEAPF32.subarray(this.positionBuffer / 4, this.positionBuffer / 4 + positionCount * 3);
 		this.normals = this.moduleInstance.HEAPF32.subarray(this.normalBuffer / 4, this.normalBuffer / 4 + positionCount * 3);
-		this.faces = this.moduleInstance.HEAPU32.subarray(this.faceBuffer / 4, this.faceBuffer / 4 + faceCount);
-		this.groups = this.moduleInstance.HEAP32.subarray(this.groupBuffer / 4, this.groupBuffer / 4 + this.chunkCount * this.chunkCount * 2);
-
-		console.log(">>> positions: ", this.positions);
+		this.faces = this.moduleInstance.HEAPU32.subarray(this.indexBuffer / 4, this.indexBuffer / 4 + faceCount);
+		this.vertexRanges = this.moduleInstance.HEAP32.subarray(
+			this.chunkVertexRangeBuffer / 4, this.chunkVertexRangeBuffer / 4 + this.chunkCount * this.chunkCount * 2);
+		this.indexRanges = this.moduleInstance.HEAP32.subarray(
+			this.chunkIndexRangeBuffer / 4, this.chunkIndexRangeBuffer / 4 + this.chunkCount * this.chunkCount * 2);
 
 		this.geometry = new THREE.BufferGeometry();
 
@@ -79,10 +83,12 @@ export class TerrainManager {
 		this.geometry.clearGroups();
 
 		for (let i = 0; i < this.chunkCount * this.chunkCount; i++) {
-			this.geometry.addGroup(this.groups[2 * i], this.groups[2 * i + 1], 0);
+			this.geometry.addGroup(this.indexRanges[2 * i], this.indexRanges[2 * i + 1], 0);
 		}
 
-		this.mesh = new THREE.Mesh(this.geometry, [new THREE.MeshLambertMaterial({ color: 0xff0000 })]);
+		this.mesh = new THREE.Mesh(
+			this.geometry, [new THREE.MeshLambertMaterial({ color: 0xff0000, wireframe: false })]
+		);
 	}
 
 	_calculateTargetChunks() {
@@ -113,50 +119,94 @@ export class TerrainManager {
 		// console.log(">>> current ids: ", this.currentChunkIds);
 		// console.log(">>> target ids: ", this.targetChunkIds);
 
-		let chunkIdToAdd = this.targetChunkIds.filter(id => !this.currentChunkIds.includes(id)).at(0);
+		let chunkIdToAdd = this.targetChunkIds.filter(
+			id => !this.currentChunks.map(v => v.chunkId).includes(id)
+		).at(0);
+
+		let chunksToRemove = this.currentChunks.filter(chunk => !this.targetChunkIds.includes(chunk.chunkId));
+
+		chunksToRemove.forEach(chunk => {
+			this.moduleInstance._deallocateChunk(
+				chunk.slots[0], chunk.slots[1], this.chunkCount ** 2,
+				this.chunkVertexRangeBuffer,
+				this.vertexFreeRangeBuffer,
+				this.chunkIndexRangeBuffer,
+				this.indexFreeRangeBuffer
+			);
+		});
+
+		this.currentChunks = this.currentChunks.filter(chunk => this.targetChunkIds.includes(chunk.chunkId));
 
 		if (!!chunkIdToAdd) {
-			let chunkIdToRemove = this.currentChunkIds.filter(id => !this.targetChunkIds.includes(id)).at(0);
-			let index = this.currentChunkIds.indexOf(chunkIdToRemove);
-			this.currentChunkIds[index] = chunkIdToAdd;
+			let gridId = chunkIdToAdd.split(':');
+
+			let slotsPtr = this.moduleInstance._allocateChunk(
+				this.positionBuffer, this.normalBuffer, this.indexBuffer,
+				this.chunkVertexRangeBuffer,
+				this.vertexFreeRangeBuffer,
+				this.chunkIndexRangeBuffer,
+				this.indexFreeRangeBuffer,
+				gridId[0] * this.chunkSize, 0, gridId[1] * this.chunkSize,
+				this.chunkSize, this.segment, this.chunkCount ** 2
+			);
+
+			let slots = this.moduleInstance.HEAP32.slice(slotsPtr / 4, slotsPtr / 4 + 2);
+
+			this.moduleInstance._doFree(slotsPtr);
+
+			this.currentChunks.push({ slots: slots, chunkId: chunkIdToAdd });
+
+			this._updateChunkGeometry(slots);
+
+			// let chunkIdToRemove = this.currentChunkIds.filter(id => !this.targetChunkIds.includes(id)).at(0);
+			// let index = this.currentChunkIds.indexOf(chunkIdToRemove);
+			// this.currentChunkIds[index] = chunkIdToAdd;
 
 			// console.log(">>> to allocate: ", chunkIdToAdd);
 			// console.log(">>> to deallocate: ", chunkIdToRemove);
 			// console.log(">>> index: ", index);
 
-			let gridId = chunkIdToAdd.split(':');
-
-			this._updateChunkGeometry(index, new THREE.Vector3(gridId[0] * this.chunkSize, 0, gridId[1] * this.chunkSize));
+			// this._updateChunkGeometry(index, new THREE.Vector3(gridId[0] * this.chunkSize, 0, gridId[1] * this.chunkSize));
 		}
 
 	}
 
-	_updateChunkGeometry(index, chunkOrigin) {
+	_updateChunkGeometry(slots) {
+		// const vertexStride = this.segment * this.segment * this.vertexStrideParam;
+		// const faceStride = this.segment * this.segment * this.faceStrideParam;
 
-		console.log(">>> update chunk index: ", index);
+		// this.moduleInstance._updateChunk(
+		// 	this.positionBuffer, this.normalBuffer, this.indexBuffer, this.chunkIndexRangeBuffer,
+		// 	chunkOrigin.x, chunkOrigin.y, chunkOrigin.z,
+		// 	this.chunkSize, this.segment, index, index * vertexStride, index * faceStride
+		// );
 
-		const vertexStride = this.segment * this.segment * this.vertexStrideParam;
-		const faceStride = this.segment * this.segment * this.faceStrideParam;
-
-		this.moduleInstance._updateChunk(
-			this.positionBuffer, this.normalBuffer, this.faceBuffer, this.groupBuffer,
-			chunkOrigin.x, chunkOrigin.y, chunkOrigin.z,
-			this.chunkSize, this.segment, index, index * vertexStride, index * faceStride
-		);
-
-		this.indexAttribute.updateRange = { offset: index * faceStride, count: faceStride };
+		this.indexAttribute.updateRange = {
+			offset: this.indexRanges[slots[1] * 2],
+			count: this.indexRanges[slots[1] * 2 + 1]
+		};
 		this.indexAttribute.needsUpdate = true;
 
-		this.positionAttribute.updateRange = { offset: index * vertexStride * 3, count: vertexStride * 3 };
+		this.positionAttribute.updateRange = {
+			offset: this.vertexRanges[slots[0] * 2] * 3,
+			count: this.vertexRanges[slots[0] * 2 + 1] * 3,
+		};
 		this.positionAttribute.needsUpdate = true;
 
-		this.normalAttribute.updateRange = { offset: index * vertexStride * 3, count: vertexStride * 3 };
+		this.normalAttribute.updateRange = {
+			offset: this.vertexRanges[slots[0] * 2] * 3,
+			count: this.vertexRanges[slots[0] * 2 + 1] * 3,
+		};
 		this.normalAttribute.needsUpdate = true;
 
 		this.geometry.clearGroups();
 
 		for (let i = 0; i < this.chunkCount * this.chunkCount; i++) {
-			this.geometry.addGroup(this.groups[2 * i], this.groups[2 * i + 1], 0);
+			this.geometry.addGroup(
+				this.indexRanges[i * 2],
+				this.indexRanges[i * 2 + 1],
+				0
+			);
 		}
 	}
 
